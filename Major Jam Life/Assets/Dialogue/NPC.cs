9fb33f04ee7ad6d3a43b619873443a0a1b59a737
GarrickC23 <garrickchiu0723@gmail.com>
2023-07-25 18:00:33 -0700

    Implemented Dialogue Description

--- Major Jam Life/Assets/Dialogue/NPC.cs
+++ Major Jam Life/Assets/Dialogue/NPC.cs
@@ -8,20 +8,44 @@ public class NPC : MonoBehaviour
     // Start is called before the first frame update
     public DialogueRunner dialogueRunner;
     public string conversationStartNode;
+    public string firstNode;
+    public string secondNode;
+    public string thirdNode;
+    public string fourthNode;
 
     void Awake()
     {
         dialogueRunner = FindObjectOfType<DialogueRunner>();
     }
 
-    private void StartConversation()
+    // private void StartConversation()
+    // {
+    //     dialogueRunner.StartDialogue(conversationStartNode);
+    // }
+
+    public void firstDescription()
     {
-        dialogueRunner.StartDialogue(conversationStartNode);
+        dialogueRunner.StartDialogue(firstNode);
+    }
+
+    public void secondDescription()
+    {
+        dialogueRunner.StartDialogue(secondNode);
+    }
+
+    public void thirdDescription()
+    {
+        dialogueRunner.StartDialogue(thirdNode);
+    }
+
+    public void fourthDescription()
+    {
+        dialogueRunner.StartDialogue(fourthNode);
     }
 
     // Update is called once per frame
     void Update()
     {
-        StartConversation();
+        //StartConversation();
     }
 }
